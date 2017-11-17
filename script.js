8053f598220dd034a7eb18297bddfa6818a6e969
Natalie Montoya <nataliesbarron@gmail.com>
2017-11-17 12:49:53 -0700

    Enabled the submit button and cleared inputs after submit

--- script.js
+++ script.js
@@ -3,10 +3,28 @@ var $price = $('#item-price');
 var $submit = $('.submit-button');
 var $wishList = $('.wish-list');
 
-$submit.on('click', addToWishList)
+$('input').on('keyup', enableSubmit);
+$submit.on('click', addToWishList);
+
+function enableSubmit() {
+	if($item.val() === '' && $price.val() !== '') {
+		$submit.prop('disabled', true);
+	}
+	else if($item.val() !== '' && $price.val() === '') {
+		$submit.prop('disabled', true);
+	}
+	else {
+		$submit.prop('disabled', false);
+	}
+}
 
 function addToWishList() {
 	event.preventDefault();
-	$wishList.append('<ul class="wish-item"><li>ITEM: ' + $item.val() + '</li>' + '<li>PRICE: $' + $price.val() + '</li>' +
+	var id = $.now();
+	$wishList.append('<ul id="${id}" class="wish-item"><li>ITEM: ' + $item.val() + '</li>' + '<li>PRICE: $' + $price.val() + '</li>' +
+		'<li>DESIRE: like</li>' +
 		'<span class="list-btns"><div class="total">Total</div><div class="delete">Delete</div><div class="purchased">Purchased</div></span></ul>')
+	$item.val('');
+	$price.val('');
+	$item.focus();
 }
